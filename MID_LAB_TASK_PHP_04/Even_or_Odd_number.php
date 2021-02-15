31eb07a4b20598778b3e65b587cc2b74c9d7cbca
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2021-02-15 19:18:35 +0600

    Finding even or odd number

---
+++ MID_LAB_TASK_PHP_04/Even_or_Odd_number.php
@@ -0,0 +1,16 @@
+<?php
+
+
+  $a=20;
+
+  if($a%2===0)
+  {
+    echo("The number is:".$a."\n");
+    echo("The number is even");
+  }else{
+    echo("The number is:".$a."\n");
+    echo("The number is odd");
+  }
+
+
+?>
