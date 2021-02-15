7f367ee8f24842958851d7a7f301fc0304ea4fda
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2021-02-15 18:29:18 +0600

    Calculating Area and Perimeter of a Rectangle

---
+++ MID_LAB_TASK_PHP_04/Rectangle.php
@@ -0,0 +1,11 @@
+<?php
+
+  $length=10;
+  $width=6;
+  $area=$length*$width;
+  $perimeter=2*($length+$width);
+  echo("The Area of Rectangle is:".$area."\n");
+  echo("The Perimeter of Rectangle is:".$perimeter);
+
+
+?>
