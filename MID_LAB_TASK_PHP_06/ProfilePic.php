9aef984812041eb17334e6be7f70ee545a59f8c2
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2021-02-23 12:48:20 +0600

    Profile Picture page design

---
+++ MID_LAB_TASK_PHP_06/ProfilePic.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>HTML Site</title>
+    </head>
+    <body>
+        <form class="" action="" method="post">
+            <fieldset>
+              <legend>PROFILE PICTURE</legend>
+              <img src="Screenshot (1272).png"><br>
+              <input type="file" id="photo" name="photo"><br><br>
+              <input type="submit" name="submit" value="submit">
+            </fieldset>
+      
+          </form>
+    </body>
+</html>
