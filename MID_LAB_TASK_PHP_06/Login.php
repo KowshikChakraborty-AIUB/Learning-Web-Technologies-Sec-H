f4f373a8510884afcda879758ab1f823a8b67c4b
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2021-02-23 12:46:31 +0600

    Login page design

---
+++ MID_LAB_TASK_PHP_06/Login.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>HTML Site</title>
+    </head>
+    <body>
+        <fieldset>
+            <legend>LOGIN</legend>
+        <form class="" action="" method="post">
+            User Name: 
+            <input type="text" name="name" value=""><br>
+            Password: 
+            <input type="password" name="password" value=""><br>
+            <input type="checkbox" name="box" value="box"> Remember Me<br>
+            <input type="submit" name="submit" value="Submit"><br>
+        </form>
+        </fieldset>
+    </body>
+</html>
