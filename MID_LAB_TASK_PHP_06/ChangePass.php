1d3d045e4fbb846d97e912f5694241ff11719c8a
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2021-02-23 12:47:35 +0600

    Password Change page design

---
+++ MID_LAB_TASK_PHP_06/ChangePass.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>HTML Site</title>
+    </head>
+    <body>
+        <fieldset>
+            <legend>CHANGE PASSWORD</legend>
+        <form class="" action="" method="post">
+            Current Password:
+            <input type="password" name="password" value=""><br>
+            New Password:
+            <input type="password" name="password" value=""><br>
+            Retype New Password:
+            <input type="password" name="password" value=""><br>
+            <input type="submit" name="submit" value="Submit"><br>
+        </form>
+        </fieldset>
+    </body>
+</html>
