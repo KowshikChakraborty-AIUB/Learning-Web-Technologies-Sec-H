e0546c4acb4727536bf004d395e27437e1620b9b
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2021-02-24 17:03:54 +0600

    Password change page design and validation

--- MID_LAB_TASK_PHP_06/ChangePass.php
+++ MID_LAB_TASK_PHP_06/ChangePass.php
@@ -1,3 +1,31 @@
+<?php
+
+
+
+	if(isset($_POST['submit']))
+	{
+
+		$pass = $_POST['password'];
+        $newpass =$_POST['newpassword'];
+        $repass = $_POST['repassword'];
+
+		if($pass == "" and $newpass == "" and $repass == ""){
+			echo "null submission";
+    }elseif($pass != $newpass and $newpass == $repass){
+		 	echo "Password is successfully changed!!";
+		}else {
+      echo "Invalid Password";
+    }
+	}
+
+
+?>
+
+
+
+
+
+
 
 <!DOCTYPE html>
 <html>
@@ -8,12 +36,21 @@
         <fieldset>
             <legend>CHANGE PASSWORD</legend>
         <form class="" action="" method="post">
-            Current Password:
-            <input type="password" name="password" value=""><br>
-            New Password:
-            <input type="password" name="password" value=""><br>
-            Retype New Password:
-            <input type="password" name="password" value=""><br>
+            <table>
+            <tr>
+            <td>Current Password:</td>
+            <td><input type="password" name="password" value=""></td>
+            </tr>
+            <tr>
+            <td>New Password:</td>
+            <td><input type="password" name="newpassword" value=""></td>
+            </tr>
+            <tr>
+            <td>Retype New Password:</td>
+            <td><input type="password" name="repassword" value=""></td>
+            </tr>
+            </table>
+            <hr>
             <input type="submit" name="submit" value="Submit"><br>
         </form>
         </fieldset>
