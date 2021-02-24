4fde9c00ede2e67b222478aa89aeecd09e94d061
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2021-02-24 17:13:48 +0600

    Profile picture page design and validation

--- MID_LAB_TASK_PHP_06/ProfilePic.php
+++ MID_LAB_TASK_PHP_06/ProfilePic.php
@@ -1,3 +1,37 @@
+<?php
+
+
+
+	if(isset($_POST['submit']))
+	{
+
+		$pic = $_POST['photo'];
+    
+    if(exif_imagetype($pic) == IMAGETYPE_JPEG || exif_imagetype($pic) == IMAGETYPE_PNG)
+    {
+      echo "Valid image format.  ";
+    }
+    else{
+      echo "Image should be in jpeg or jpg or png format";
+    }
+    
+    
+    
+    if(filesize($pic) < 4194304)
+    {
+      true;
+    }else {
+      echo "Image Should be less then 4 MB.";
+    }
+	}
+
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -8,7 +42,8 @@
             <fieldset>
               <legend>PROFILE PICTURE</legend>
               <img src="Screenshot (1272).png"><br>
-              <input type="file" id="photo" name="photo"><br><br>
+              <input type="file" id="photo" name="photo"><br>
+              <hr>
               <input type="submit" name="submit" value="submit">
             </fieldset>
       
