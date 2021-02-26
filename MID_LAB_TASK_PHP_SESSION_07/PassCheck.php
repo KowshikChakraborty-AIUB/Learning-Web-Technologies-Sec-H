590d9eb79c94d778066ecd511a0c58b78e506f0b
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2021-02-26 22:42:28 +0600

    Checking Change Password page with php validation

---
+++ MID_LAB_TASK_PHP_SESSION_07/PassCheck.php
@@ -0,0 +1,32 @@
+<?php
+
+    session_start();
+
+	if(isset($_POST['submit']))
+	{
+
+		$pass = $_POST['password'];
+        $newpass =$_POST['newpassword'];
+        $repass = $_POST['repassword'];
+
+		if($pass == "" and $newpass == "" and $repass == ""){
+			echo "null submission";
+    }elseif($pass != $newpass and $newpass == $repass){
+        $userr = [	
+            'password'=>$pass, 
+            'newpassword'=>$newpass,
+            'repassword'=> $repass
+        ];
+
+//$_SESSION['username'] = $username;
+//$_SESSION['password'] = $password;
+$_SESSION['current_user'] = $userr;
+
+header('location: ChangePass.php');
+		}else {
+      echo "Invalid Password";
+    }
+	}
+
+
+?>
