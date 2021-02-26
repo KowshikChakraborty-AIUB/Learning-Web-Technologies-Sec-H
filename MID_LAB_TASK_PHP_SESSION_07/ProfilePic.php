c4233d83cc60ebaa3e2379ee591d46a2e4895809
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2021-02-26 22:45:15 +0600

    Profile Picutre Change page html design with php code

---
+++ MID_LAB_TASK_PHP_SESSION_07/ProfilePic.php
@@ -0,0 +1,75 @@
+<?php
+	session_start();
+	if(isset($_SESSION['flag']))
+	{
+?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Public Home</title>
+  </head>
+  <body>
+    <table border="1" cellspacing="0" width="80%" >
+      <tr>
+         <td colspan="2">
+          <table width="100%">
+              <tr>
+                <td><img height="40px" weight="40px" src="logo.png" alt=""></td>
+                <td align = "right">
+                    Logged in as
+                    <a href="ViewProfile.php"><?php $userr = $_SESSION['current_user']; $name= $userr['name']; echo "$name"; ?></a>|
+                    <a href="UserLogout.php">Logout</a>
+                </td>
+              </tr>
+          </table>
+          </td>
+      </tr>
+      <tr>
+        <td height="150px" weight="150px">
+            <b>Account</b>
+            <hr>
+                <ul>
+                    <li><a href="Dashboard.php">Dashboard</a></li>
+                    <li><a href="ViewProfile.php">View Profile</a></li>
+                    <li><a href="EditProfile.php">Edit Profile</a></li>
+                    <li><a href="ProfilePic.php">Change Profile Picture</a></li>
+                    <li><a href="ChangePass.php">Change Password</a></li>
+                    <li><a href="UserLogout.php">Logout</a></li>
+                </ul>
+
+        </td>
+        <td>
+            <fieldset>
+                <form class="" action="" method="post">
+                    <fieldset>
+                      <legend>PROFILE PICTURE</legend>
+                      <img height="100 px" width="100 px" src="user.png"><br>
+                      <input type="file" id="photo" name="photo"><br>
+                      <hr>
+                      <input type="submit" name="submit" value="submit">
+                    </fieldset>
+              
+                  </form>
+            </fieldset>
+        </td>
+      </tr>
+      <tr>
+        <td align="center" colspan="2">Copyright Ⓒ 2017</td>
+      </tr>
+    </table>
+
+  </body>
+</html>
+
+
+<?php
+
+	}else{
+		header('location: Login.html');
+	}
+
+?>
